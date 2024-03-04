31a5a3f256263dc4141bf992db329eb7976bd20d
qseudo <57642143+yg-lim@users.noreply.github.com>
2024-03-04 07:27:12 -0500

    Initial commit

---
+++ part1/src/App.jsx
@@ -0,0 +1,24 @@
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
+    </div>
+  )
+};
+
+const App = () => {
+  const name = "Peter";
+  const age = 10;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="George" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Hello />
+    </div>
+  );
+}
+
+export default App
