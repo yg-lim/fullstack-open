f34726f467ff8764cdbb929bce1875ce40cfeb64
qseudo <57642143+yg-lim@users.noreply.github.com>
2024-03-06 10:51:46 -0500

    completes 2.11

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,57 +1,26 @@
-import { Course } from "./components/course";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Note } from './components/Note'
+
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ];
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-  return (
-    <>
-      <h1>Web development curriculum</h1>
-      {courses.map(course => <Course key={course.name} course={course}/>)}
-    </>
-  )
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+  // ...
 }
 
 export default App;
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ handleChange, text }) => {
   return (
@@ -31,16 +33,21 @@ const Persons = ({ displayedPersons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [displayedPersons, setDisplayedPersons] = useState(persons);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data);
+        setDisplayedPersons(response.data);
+    })
+      .catch(error => console.log(error));
+  }, []);
 
   function contactExists(name) {
     return persons.find(person => person.name.toLowerCase() === name.toLowerCase());
