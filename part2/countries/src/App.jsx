2a2e70c09b51a33a309e1e9405e83659ea0e601d
qseudo <57642143+yg-lim@users.noreply.github.com>
2024-03-12 11:04:14 -0400

    completes 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
+const apiKey = import.meta.env.VITE_OPEN_WEATHER_KEY;
+const KELVINS_TO_C = 273.15;
+
 function Results({ results, handleButton }) {
-  let content;
   if (results.length > 10)
-    content = "Too many matches, specify another filter.";
-  else if (results.length === 1) content = <Country country={results[0]} />;
-  else
-    content = results.map((result) => {
+    return <p>Too many matches, specify another filter.</p>;
+  else {
+    const content = results.map((result) => {
       return (
         <li key={result.cca2}>
           {result.name.common}
@@ -19,10 +20,13 @@ function Results({ results, handleButton }) {
       );
     });
 
-  return <div>{content}</div>;
+    return <div>{content}</div>;
+  }
 }
 
-function Country({ country }) {
+function Country({ country, weather }) {
+  if (!country) return null;
+
   return (
     <>
       <h1>{country.name.common}</h1>
@@ -35,6 +39,22 @@ function Country({ country }) {
         ))}
       </ul>
       <img src={country.flags.png} alt={country.flags.alt} />
+      {weather && <Weather capital={country.capital} weather={weather} />}
+    </>
+  );
+}
+
+function Weather({ weather, capital }) {
+  return (
+    <>
+      <h2>Weather in {capital}</h2>
+      <p>
+        temperature {(weather.main.temp - KELVINS_TO_C).toFixed(2)}° Celsius
+      </p>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+      />
+      <p>wind {weather.wind.speed} m/s</p>
     </>
   );
 }
@@ -43,6 +63,25 @@ function App() {
   const [filter, setFilter] = useState("");
   const [results, setResults] = useState([]);
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  function fetchWeather(country) {
+    axios
+      .get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${country.capital},${country.cioc}&limit=1&appid=${apiKey}`,
+      )
+      .then((geoResponse) => {
+        const geoData = geoResponse.data[0];
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${geoData.lat}&lon=${geoData.lon}&appid=${apiKey}`,
+          )
+          .then((weatherResponse) => {
+            setWeather(weatherResponse.data);
+          });
+      });
+  }
 
   function handleFilterChange(event) {
     const input = event.target.value;
@@ -54,15 +93,24 @@ function App() {
         )
       : [];
 
-    setResults(matches);
+    if (matches.length === 1) {
+      if (country === null) {
+        setCountry(matches[0]);
+        fetchWeather(matches[0]);
+      }
+    } else {
+      setResults(matches);
+      setCountry(null);
+    }
   }
 
   function handleButtonClick(event) {
     const countryToDisplay = results.filter(
       (country) => country.cca2 === event.target.dataset.cca2,
-    );
+    )[0];
 
-    setResults(countryToDisplay);
+    setCountry(countryToDisplay);
+    fetchWeather(countryToDisplay);
   }
 
   useEffect(() => {
@@ -75,8 +123,13 @@ function App() {
 
   return (
     <>
-      find countries <input value={filter} onChange={handleFilterChange} />
-      <Results results={results} handleButton={handleButtonClick} />
+      find countries{" "}
+      <input autoFocus value={filter} onChange={handleFilterChange} />
+      {country ? (
+        <Country country={country} weather={weather} />
+      ) : (
+        <Results results={results} handleButton={handleButtonClick} />
+      )}
     </>
   );
 }
