26b9648557b202c8f0ecd53e5fcee3eacc056d65
qseudo <57642143+yg-lim@users.noreply.github.com>
2024-03-13 10:28:39 -0400

    completes courseinfo

---
+++ part3/courseinfo/index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+function generateId() {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+  return maxId + 1;
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = +req.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (note) res.json(note);
+  else res.status(404).end();
+});
+
+app.post("/api/notes/", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: !!body.important,
+    id: generateId(),
+  };
+
+  notes.push(note);
+  res.json(note);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = +req.params.id;
+
+  if (id) {
+    notes = notes.filter((note) => note.id === id);
+    res.status(204).end();
+  } else res.status(404).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
