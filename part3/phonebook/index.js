3d72c24b9b364cd41f3e146a57537a0801cf4332
qseudo <57642143+yg-lim@users.noreply.github.com>
2024-03-13 11:14:29 -0400

    completes 3.3-3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+function generateId() {
+  return Math.floor(Math.random() * 99999);
+}
+
+function nameExists(personName) {
+  return persons.find(
+    (person) =>
+      person.name.trim().toLowerCase() === personName.trim().toLowerCase(),
+  );
+}
+
 const persons = [
   {
     id: 1,
@@ -32,6 +45,40 @@ app.get("/info", (req, res) => {
   const info = `<p>Phonebook has info for ${persons.length} people.`;
   const date = new Date().toUTCString();
   res.send(info + `<p>${date}</p>`);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = +req.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (!person) res.status(404).end();
+  else res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = +req.params.id;
+  const idxOfPerson = persons.findIndex((person) => person.id === id);
+  if (idxOfPerson !== -1) persons.splice(idxOfPerson, 1);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const person = req.body;
+
+  if (!person.name || !person.number) {
+    res.status(400).json({
+      error: "person must have name and number",
+    });
+  } else if (nameExists(person.name)) {
+    res.status(400).json({
+      error: "name must be unique",
+    });
+  } else {
+    person.id = generateId();
+    persons.push(person);
+    res.send(person);
+  }
 });
 
 const PORT = 3001;
