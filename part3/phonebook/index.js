e71a9de63db10438b6b2b55cf24773f5503f65a7
qseudo <57642143+yg-lim@users.noreply.github.com>
2024-03-13 10:57:21 -0400

    completes 3.1-3.2

---
+++ part3/phonebook/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+const persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons/", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const info = `<p>Phonebook has info for ${persons.length} people.`;
+  const date = new Date().toUTCString();
+  res.send(info + `<p>${date}</p>`);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
