93fee2304ca5ef2e34577e1ec7e48402f940256c
qseudo <57642143+yg-lim@users.noreply.github.com>
2024-03-14 09:21:53 -0400

    sets environment variables for phonebook-backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -96,7 +96,7 @@ const unknownEndpoint = (_req, res) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
