a68c8a1c3011e744b6a71803902ac039d12b0509
qseudo <57642143+yg-lim@users.noreply.github.com>
2024-03-14 09:33:04 -0400

    buildui

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -39,6 +39,7 @@ function nameExists(personName) {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname + "/dist"));
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
